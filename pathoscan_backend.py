ad60bf7c25375740106e5f8db8910e728267b043
Akarsh Ghildyal <akarshghildyal@gmail.com>
2025-04-14 17:30:36 +0530

    update : change to gemini2.0-flash  from gemini

--- pathoscan_backend.py
+++ pathoscan_backend.py
@@ -9,7 +9,7 @@ import streamlit as st
 load_dotenv()
 
 llm = ChatOpenAI(
-    model="google/gemini-2.5-pro-exp-03-25:free",
+    model="google/gemini-2.0-flash-thinking-exp-1219:free",
     openai_api_base="https://openrouter.ai/api/v1",
     openai_api_key=st.secrets["OPENROUTER_API_KEY"]
 )
